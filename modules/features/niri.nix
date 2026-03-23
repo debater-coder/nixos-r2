a1a580b8a6554caaa9ed4e75d05a39db307a90d3
debater-coder <hamzah@syedahmed.net>
2026-03-23 23:45:09 +1100

    done

--- modules/features/niri.nix
+++ modules/features/niri.nix
@@ -30,6 +30,7 @@
           input.keyboard.xkb.layout = "us,ua";
 
           layout.gaps = 5;
+          input.focus-follows-mouse = null;
 
           binds = {
             "Mod+Return".spawn = lib.getExe pkgs.kitty;
@@ -55,31 +56,20 @@
             "Mod+Shift+K".move-window-up = null;
             "Mod+Shift+J".move-window-down = null;
 
-            "Mod+1".focus-workspace = "w0";
-            "Mod+2".focus-workspace = "w1";
-            "Mod+3".focus-workspace = "w2";
-            "Mod+4".focus-workspace = "w3";
-            "Mod+5".focus-workspace = "w4";
-            "Mod+6".focus-workspace = "w5";
-            "Mod+7".focus-workspace = "w6";
-            "Mod+8".focus-workspace = "w7";
-            "Mod+9".focus-workspace = "w8";
-            "Mod+0".focus-workspace = "w9";
+            "Mod+Shift+S".screenshot = null;
 
-            "Mod+Shift+1".move-column-to-workspace = "w0";
-            "Mod+Shift+2".move-column-to-workspace = "w1";
-            "Mod+Shift+3".move-column-to-workspace = "w2";
-            "Mod+Shift+4".move-column-to-workspace = "w3";
-            "Mod+Shift+5".move-column-to-workspace = "w4";
-            "Mod+Shift+6".move-column-to-workspace = "w5";
-            "Mod+Shift+7".move-column-to-workspace = "w6";
-            "Mod+Shift+8".move-column-to-workspace = "w7";
-            "Mod+Shift+9".move-column-to-workspace = "w8";
-            "Mod+Shift+0".move-column-to-workspace = "w9";
 
             "Alt+Space".spawn-sh = "${
               (lib.getExe self.packages.${pkgs.stdenv.hostPlatform.system}.myNoctalia)
             } ipc call launcher toggle";
+
+            "Super+Delete".spawn-sh = "${
+              (lib.getExe self.packages.${pkgs.stdenv.hostPlatform.system}.myNoctalia)
+            } ipc call lockScreen lock";
+
+            "Super+Escape".spawn-sh = "${
+              (lib.getExe self.packages.${pkgs.stdenv.hostPlatform.system}.myNoctalia)
+            } ipc call lockScreen lock";
 
             "XF86AudioRaiseVolume".spawn-sh = "wpctl set-volume -l 1.4 @DEFAULT_AUDIO_SINK@ 5%+";
             "XF86AudioLowerVolume".spawn-sh = "wpctl set-volume -l 1.4 @DEFAULT_AUDIO_SINK@ 5%-";
@@ -94,26 +84,6 @@
             "Mod+Ctrl+WheelScrollDown".focus-workspace-down = null;
             "Mod+Ctrl+WheelScrollUp".focus-workspace-up = null;
           };
-
-          workspaces =
-            let
-              ws = {
-                layout.gaps = 5;
-              };
-            in
-            {
-              "w0" = ws;
-              "w1" = ws;
-              "w2" = ws;
-              "w3" = ws;
-              "w4" = ws;
-              "w5" = ws;
-              "w6" = ws;
-              "w7" = ws;
-              "w8" = ws;
-              "w9" = ws;
-            };
-        };
       };
     };
 }
