09f2dcb5304bb8da12538840515c1458d990bd7c
debater-coder <hamzah@syedahmed.net>
2026-06-12 10:37:24 +1000

    fix: no more home row mods

--- modules/hosts/starscream/configuration.nix
+++ modules/hosts/starscream/configuration.nix
@@ -98,26 +98,13 @@
             extraDefCfg = "process-unmapped-keys yes";
             config = ''
               (defsrc
-               caps a s d f j k l ;
-              )
-              (defvar
-               tap-time 300
-               hold-time 500
+               caps
               )
               (defalias
                caps esc
-               a (tap-hold-release-timeout $tap-time $hold-time a lmet a)
-               s (tap-hold-release-timeout $tap-time $hold-time s lalt s)
-               d (tap-hold-release-timeout $tap-time $hold-time d lsft d)
-               f (tap-hold-release-timeout $tap-time $hold-time f lctl f)
-               j (tap-hold-release-timeout $tap-time $hold-time j rctl j)
-               k (tap-hold-release-timeout $tap-time $hold-time k rsft k)
-               l (tap-hold-release-timeout $tap-time $hold-time l ralt l)
-               ; (tap-hold-release-timeout $tap-time $hold-time ; rmet ;)
               )
-
               (deflayer base
-               @caps @a  @s  @d  @f  @j  @k  @l  @;
+               @caps
               )
             '';
           };
